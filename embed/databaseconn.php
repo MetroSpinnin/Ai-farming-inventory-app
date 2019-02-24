c7169375d234d6692a4800623f12a2cf63f283f8
Samuel Jim Nnamdi <46245794+MetroSpinnin@users.noreply.github.com>
2019-02-24 17:54:02 +0100

    updated database connection file
    An extended comment on how the database connection files should be  handled was explicitly specified

--- embed/databaseconn.php
+++ embed/databaseconn.php
@@ -1,5 +1,12 @@
 <?php 
 
+/*
+Add your own details  here
+which includes the hosting account
+database,username and password
+My password field was left empty because
+i dont use a password for localhost
+*/
 $host = "localhost";
 $user = "root";
 $pass = "";
